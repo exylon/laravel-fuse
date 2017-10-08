64be849e2d47506d6bb85cce23bd986314bc970d
Edmandie Samonte <edmandie.samonte@gmail.com>
2017-10-08 15:18:48 +0800

    Added helpful macros

--- src/FuseServiceProvider.php
+++ src/FuseServiceProvider.php
@@ -4,6 +4,9 @@
 namespace Exylon\Fuse;
 
 
+use Exylon\Fuse\Support\Attributes;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Http\Request;
 use Illuminate\Support\ServiceProvider;
 
 class FuseServiceProvider extends ServiceProvider
@@ -18,6 +21,8 @@ class FuseServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->registerArtisanCommands();
         }
+
+        $this->registerMacros();
     }
 
     /**
@@ -32,5 +37,52 @@ class FuseServiceProvider extends ServiceProvider
             Console\ServiceMakeCommand::class,
             Console\RepositoryMakeCommand::class,
         ]);
+    }
+
+    /**
+     * Register macros
+     *
+     * @return void
+     */
+    private function registerMacros()
+    {
+        Builder::macro('forceMake', function ($attributes) {
+            return $this->newModelInstance()->forceFill($attributes);
+        });
+
+
+        $hasGeoip = class_exists('Torann\GeoIP\GeoIP');
+        $hasAgent = class_exists('Jenssegers\Agent\Agent');
+
+        if ($hasGeoip) {
+            Request::macro('location', function () {
+                if ($this->location) {
+                    return $this->location;
+                }
+                return $this->location = new Attributes(geoip($this->getClientIp())->toArray());
+            });
+        }
+
+        if ($hasAgent) {
+            Request::macro('agent', function () {
+                if ($this->agent) {
+                    return $this->agent;
+                }
+                $agent = new \Jenssegers\Agent\Agent($this->server->all());
+                return $this->agent = new Attributes([
+                    'agent'      => $agent->getUserAgent(),
+                    'is_mobile'  => $agent->isMobile(),
+                    'is_phone'   => $agent->isPhone(),
+                    'is_tablet'  => $agent->isTablet(),
+                    'device'     => $agent->device(),
+                    'is_desktop' => $agent->isDesktop(),
+                    'platform'   => $agent->platform(),
+                    'is_robot'   => $agent->isRobot(),
+                    'robot'      => $agent->robot(),
+                    'browser'    => $agent->browser(),
+                    'languages'  => $agent->languages()
+                ]);
+            });
+        }
     }
 }
